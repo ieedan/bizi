7cf3f4485bdeec370aeb4954815c4ee71d080986
Aidan Bleser <aidanbleser35@gmail.com>
2026-02-16 07:47:53 -0600

    add site

---
+++ apps/www/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Snippet } from "$lib/components/ui/snippet";
+</script>
+
+<main class="h-dvh flex items-center justify-center"> 
+    <div class="max-w-xl w-full">
+        <h1 class="text-4xl font-bold text-center">bizi</h1>
+        <p class="mb-4 text-center text-muted-foreground">Manage dependent concurrent tasks with sanity.</p>
+        <Snippet text="curl -fsSL https://getbizi.dev/install | bash" />
+    </div>
+</main>
---
+++ apps/www/src/routes/install.sh/+server.ts
@@ -0,0 +1,5 @@
+import { redirect } from "@sveltejs/kit";
+
+export function GET() {
+    redirect(303, "https://raw.githubusercontent.com/ieedan/bizi/main/scripts/install");
+}
